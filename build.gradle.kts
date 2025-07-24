839cc5b81e0af2bb8a16398538ea843d03a3d9e2
Stuffzi <mikey110802@icloud.com>
2025-07-23 23:32:52 -0700

    Refactored with significantly more advanced http calling, added new dependancy, better cleanliness.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,8 @@ repositories {
 }
 
 dependencies {
-   implementation("com.apptasticsoftware:rssreader:3.9.3"); //https://github.com/w3stling/rssreader
+   implementation("com.apptasticsoftware:rssreader:3.9.3") //https://github.com/w3stling/rssreader
+   implementation("com.squareup.okhttp3:okhttp:5.1.0") //https://square.github.io/okhttp/#releases
 }
 
 tasks.test {
--- src/main/java/com/FMR/Main.java
+++ src/main/java/com/FMR/Main.java
@@ -1,46 +1,77 @@
 package com.FMR;
 
+//JAVA
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+//OkHTTP
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+//RSSReader
 import com.apptasticsoftware.rssreader.RssReader;
 import com.apptasticsoftware.rssreader.Item;
 
-
-import java.net.*;
-import java.util.List;
-import java.util.stream.Stream;
 
 public class Main
 {
     public static final String NRC_CURRENT = "https://www.nrc.gov/rss/reactorstatus.xml";
 
-	public static RssReader NRC_RSS = null;
-
-    private static boolean _automaticMode = false;
-
+	public static OkHttpClient _NRC_CLIENT;
+	public static RssReader _NRC_RSSREADER;
 
     private static void updateCurrent()
     {
-		Stream<Item> reactorPowers;
+		//Contact NRC and get data
+		Request NRCHandle = new Request.Builder().url(NRC_CURRENT).build();
+		Response NRCResponse;
 		try
 		{
-			reactorPowers = NRC_RSS.read(NRC_CURRENT);
+			NRCResponse = _NRC_CLIENT.newCall(NRCHandle).execute();
 		} catch (IOException e)
 		{
-			System.out.println(e + "\n[updateCurrent] Unable to get the NRC RSS feed. Check internet and interference.");
+			System.out.println(e + "\n[updateCurrent] Unable to contact NRC, check internet or interference");
 			return;
 		}
 
-		reactorPowers.forEach(item -> {
-			System.out.println(item.getTitle().orElse("---"));
-		});
-	}
+		String NRCContent;
+		if (NRCResponse.isSuccessful())
+		{
+			try
+			{
+				NRCContent = NRCResponse.body().string();
+			}
+			catch (IOException e)
+			{
+				System.out.println(e + "\n[updateCurrent] Unable to fetch content from NRC, check internet or interference");
+				return;
+			}
+		}
+		else
+		{
+			System.out.println("NRC HTTP RESPONSE: [" + NRCResponse.code() + "]\n[updateCurrent] Non optimal NRC call." +
+					" Check URL, Internet, or contact developer");
+			return;
+		}
+		NRCResponse.close();
 
+		//Parse data and build something out of it
+		InputStream NRC_RSS = new ByteArrayInputStream(NRCContent.getBytes(StandardCharsets.UTF_8));
+		List<Item> ReactorData = _NRC_RSSREADER.read(NRC_RSS).toList();
+		System.out.println("=== REACTOR DATA ===");
+		ReactorData.forEach(Reactor ->
+				System.out.println(Reactor.getTitle().orElse("NRC Data Handling Error")));
+	}
 
 
     public static void main(String[] args)
     {
-		NRC_RSS = new RssReader();
+		_NRC_CLIENT = new OkHttpClient();
+		_NRC_RSSREADER = new RssReader();
 		updateCurrent();
     }
 }
